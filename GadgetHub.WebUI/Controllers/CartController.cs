4bc9cda966a014c6dfa5b2d5334cbfe6fad04f07
James <52940969+cplson@users.noreply.github.com>
2026-03-24 15:37:24 -0500

    updated rd2 version

--- GadgetHub.WebUI/Controllers/CartController.cs
+++ GadgetHub.WebUI/Controllers/CartController.cs
@@ -56,5 +56,15 @@ namespace GadgetHub.WebUI.Controllers
                 Cart = cart
             });
         }
+
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
     }
 }
